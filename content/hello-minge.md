2a9df7a04fc2442565ebeb21b505c12ecdaf01be
jazbogross <57679104+jazbogross@users.noreply.github.com>
2023-04-25 21:23:48 +0100

    update

---
+++ content/hello-minge.md
@@ -0,0 +1,15 @@
+---
+title: hello minge
+---
+
+![](/cross_my_heart_instagram_4x5_0-1920x.jpg)
+
+`hhekkk`
+
+```
+poo
+```
+
+```html
+<div><h1>minger</h1></div>
+```
---
+++ tina/config.ts
@@ -0,0 +1,45 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+
+export default defineConfig({
+  branch,
+  clientId: "c77846e7-fd5d-4c47-b5bd-f0cb63f84506", // Get this from tina.io
+  token: "0b794dde3d0446ff7987d6a83edbd9e0849720f0", // Get this from tina.io
+
+  build: {
+    outputFolder: "admin",
+    publicFolder: "static",
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "static",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "Posts",
+        path: "content",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+    ],
+  },
+});
