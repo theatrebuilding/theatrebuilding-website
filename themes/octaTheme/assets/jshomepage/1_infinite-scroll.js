6c2aa873245c3127cabf1c8ad5859e3d37e47fa9
Jazbo Gross <jazbogross@Jazbos-Mac-Studio.local>
2023-12-17 18:08:34 +0000

    curtain ready for mobile and desktop

--- themes/octaTheme/assets/jshomepage/1_infinite-scroll.js
+++ themes/octaTheme/assets/jshomepage/1_infinite-scroll.js
@@ -1,58 +1,77 @@
+// Function to check if the current device is a mobile device (this function is accessible within onther .js files because the .js files get bundeled by Hugo on build)
 function isMobileDevice() {
   return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1 || window.matchMedia("(max-width: 760px)").matches);
 }
 
+// Conditional execution based users device
+// If the current device is a mobile device, we don't want to do anything. We just want to keep the flex column that we currently have as it is quite simple and easy to understand on mobile.
 if ( isMobileDevice() ) {
   
 // TO-DO
-// Scrolling based on mobile device tilt, beta, gamma.
+// Maybe other things to do here in future...
+
 
 } else {
+  // ELSE IF IS DESKTOP
   const maxSpeed = 2; // Max scrolling speed in pixels
-  let mouseX = 0;
-  let mouseY = 0;
+  let mouseX = 0;     // Current mouse X position
+  let mouseY = 0;     // Current mouse Y position
 
+  // Function to apply scroll based on calculated amounts
   function applyScroll(scrollAmountX, scrollAmountY) {
+    
+    // Calculate new scroll positions
     let newScrollX = window.scrollX + scrollAmountX;
     let newScrollY = window.scrollY + scrollAmountY;
-  
+    
+    // Get viewport and document dimensions
     const viewportWidth = window.innerWidth;
     const viewportHeight = window.innerHeight;
     const bodyWidth = document.body.scrollWidth;
     const bodyHeight = document.body.scrollHeight;
-  
+    
+    // Adjust scroll position for looping scroll
     newScrollX = (newScrollX < 0) ? bodyWidth + newScrollX : (newScrollX + viewportWidth > bodyWidth) ? newScrollX - bodyWidth + viewportWidth : newScrollX;
     newScrollY = (newScrollY < 0) ? bodyHeight + newScrollY : (newScrollY + viewportHeight > bodyHeight) ? newScrollY - bodyHeight + viewportHeight : newScrollY;
-  
+    
+    // Apply calculated scroll positions
     window.scrollTo(newScrollX, newScrollY);
   }
-
+  
+  // Function to update scroll based on mouse position
   function updateScroll() {
+    
+    // Get viewport center
     const viewportWidth = window.innerWidth;
     const viewportHeight = window.innerHeight;
     const centerX = viewportWidth / 2;
     const centerY = viewportHeight / 2;
-  
+    
+    // Calculate delta for scroll direction
     const deltaX = mouseY - centerY; // Inverted scroll direction for X
     const deltaY = mouseX - centerX; // Inverted scroll direction for Y
     const distance = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
-  
+    
+    // Calculate dynamic scroll speed
     const maxDistance = Math.sqrt(centerX * centerX + centerY * centerY);
     const speed = Math.min((distance / maxDistance) * maxSpeed, maxSpeed);
-  
+    
+    // Apply scroll based on speed and direction
     const scrollX = (speed * deltaX) / distance;
     const scrollY = (speed * deltaY) / distance;
-  
     applyScroll(scrollY, scrollX);
-  
+    
+    // Continuously update scroll
     requestAnimationFrame(updateScroll);
   }
 
+  // Event listener for mouse movement
   document.addEventListener('mousemove', (event) => {
     mouseX = event.clientX;
     mouseY = event.clientY;
   });
 
+  // Delay the start of the script execution
   setTimeout(() => {
     requestAnimationFrame(updateScroll);
   }, 2000); // Delay the script execution for 2 seconds
--- themes/octaTheme/assets/jshomepage/4_draggable-top-info.js
+++ themes/octaTheme/assets/jshomepage/4_draggable-top-info.js
@@ -1,13 +1,21 @@
+// Event listener for when the DOM content is fully loaded
 document.addEventListener('DOMContentLoaded', function() {
+    
+    // Element references
     const curtain = document.getElementById('curtain');
     const footer = document.getElementById('footer');
     const arrowBtn = document.getElementById('footer-arrow');
     const burgerMenu = document.getElementById('burger-menu');
+
+    // Variables for tracking touch/mouse start positions and whether the menu has been interacted with or not
     let startY, startTop, interacted = false;
 
+    // Function to shuffle statements within the curtain element
     function shuffleStatements() {
         const statements = document.getElementsByClassName('curtain-statement');
         const statementsArray = Array.from(statements);
+        
+        // Fisher-Yates shuffle algorithm
         for (let i = statementsArray.length - 1; i > 0; i--) {
             const j = Math.floor(Math.random() * (i + 1));
             [statementsArray[i], statementsArray[j]] = [statementsArray[j], statementsArray[i]];
@@ -17,14 +25,17 @@ document.addEventListener('DOMContentLoaded', function() {
         statementsArray.forEach(statement => parent.appendChild(statement));
     }
 
+    // Function to set curtain transition with a default duration of 1 second
     function setTransition(duration = '1s') {
         curtain.style.transition = `top ease ${duration}`;
     }
 
+    // Function to unset the transition for the curtain
     function resetTransition() {
         curtain.style.transition = 'unset';
     }
 
+    // Function to resize the curtain, adjusting its position based on device type
     function resizeCurtain() {
         curtain.style.transition = 'top ease 1s';
         if (isMobileDevice()) {
@@ -33,15 +44,17 @@ document.addEventListener('DOMContentLoaded', function() {
             updateCurtainPositionDesktop();
         }
         setTimeout(function() {
-            curtain.style.transition = 'unset';
-          }, 1000); // 1-second delay
+            resetTransition();
+          }, 1000); // Resets transition after 1 second
     }  
 
+    // Function to set the initial position of the curtain based on device type
     function setInitialPosition() {
         const windowHeight = document.documentElement.clientHeight;
         if(isMobileDevice()) {
+
+            // Mobile-specific positioning and element style adjustments
             curtain.style.top = -windowHeight + 100 + 'px';
-         //   curtain.style.width = document.documentElement.clientWidth + 'px';  
             arrowBtn.style.display = 'none';
             document.getElementById('join-console-text').style.display = 'none';  
             document.getElementById('inner-curtain').style.height = document.documentElement.clientHeight - 100 + 'px';
@@ -50,6 +63,8 @@ document.addEventListener('DOMContentLoaded', function() {
               }, 1000); // 1-second delay
             curtain.style.position = 'fixed';
         } else {
+            
+            // Desktop-specific positioning and element style adjustments
             curtain.style.top = -windowHeight + 25 + 'px';
             document.getElementById('inner-curtain').style.height = document.documentElement.clientHeight - 100 + 'px';
             burgerMenu.style.display = 'none';
@@ -57,19 +72,23 @@ document.addEventListener('DOMContentLoaded', function() {
         }  
     }
 
+    // Function to update curtain position on desktop devices
     function updateCurtainPositionDesktop() {
         setInitialPosition();
 
+        // Event handlers for dragging the curtain using the footer
         footer.onmousedown = footer.ontouchstart = function(e) {
             interacted = true;
             startY = (e.touches ? e.touches[0].clientY : e.clientY);
             startTop = parseInt(window.getComputedStyle(curtain).top, 10);
 
+            // Handlers for moving the curtain as the mouse moves or touch moves
             document.onmousemove = document.ontouchmove = function(e) {
                 e.preventDefault();
                 let clientY = e.touches ? e.touches[0].clientY : e.clientY;
                 let newPos = startTop + clientY - startY;
 
+                // Limits the movement of the curtain within the window bounds
                 const windowHeight = document.documentElement.clientHeight;
                 const maxTop = windowHeight - 25 - curtain.offsetHeight;
                 if (newPos >= -windowHeight + 25 && newPos <= maxTop) {
@@ -77,6 +96,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 }
             };
 
+            // Handlers to stop moving the curtain when mouse is released or touch ends
             document.onmouseup = document.ontouchend = function() {
                 document.onmousemove = document.ontouchmove = null;
                 document.onmouseup = document.ontouchend = null;
@@ -84,15 +104,16 @@ document.addEventListener('DOMContentLoaded', function() {
         };
     }
 
+    // Function to update curtain position on mobile devices
     function updateCurtainPositionMobile() {
         if (!interacted) {
             setInitialPosition();
         } else {
             return;
         }
-    
         const welcomeText = document.getElementById('inner-curtain');
-    
+        
+        // Handler for burger menu click event
         function handleBurgerMenuClick() {
             if (!interacted) {
                 setTransition();
@@ -107,12 +128,12 @@ document.addEventListener('DOMContentLoaded', function() {
                 interacted = false;
             }
         }
-    
-        burgerMenu.addEventListener('click', handleBurgerMenuClick);
-      //  burgerMenu.addEventListener('touchend', handleBurgerMenuClick);
-    }
-    
 
+        // Event listener for burger menu click
+        burgerMenu.addEventListener('click', handleBurgerMenuClick);
+    }
+
+    // Function to make the curtain animate briefly to come into view
     function curtainPeek() {
         const windowHeight = document.documentElement.clientHeight;
         if(!interacted) {
@@ -126,7 +147,7 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 
-
+    // Conditional execution based on device type
     if (isMobileDevice()) {
         shuffleStatements();
         updateCurtainPositionMobile();
@@ -135,7 +156,7 @@ document.addEventListener('DOMContentLoaded', function() {
         window.onresize = resizeCurtain;
         updateCurtainPositionDesktop();
         shuffleStatements();
-        setTimeout(curtainPeek, 5000);
-        setInterval(curtainPeek, 30000);
+        setTimeout(curtainPeek, 5000); // Curtain peeks after 5 seconds
+        setInterval(curtainPeek, 30000); // Repeats every 30 seconds
     }
 });
